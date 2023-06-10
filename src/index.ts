0f752d558cd74d35779b94bfb43c51aac4e9cabe
Aser <adocteur@gmail.com>
2023-06-10 15:51:14 -0400

    Testing workflow

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Vehicle {
+	make: string;
+	model: string;
+	year: number;
+}
+
+const sedan: Vehicle = {
+	make: "ford",
+	model: "Escape",
+	year: 2023,
+};
+
+console.log(sedan);
+
+export default sedan;
