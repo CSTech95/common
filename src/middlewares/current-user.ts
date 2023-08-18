289c07bfda8048cfe87938f73aff4267c2460197
Aser <adocteur@gmail.com>
2023-08-18 13:08:42 -0400

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,27 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { UserPayload } from "../types";
+//import { UserPayload } from "../types";
 
-//interface UserPayload {
-//	id: string | null;
-//	fName?: string | null;
-//	lName: string | null;
-//	email: string | null;
-//	otherInfo: {
-//		address: string | null;
-//		state: string | null;
-//		zip: string | null;
-//		createdAt: string | null;
-//	};
-//}
+interface UserPayload {
+	id: string | null;
+	fName?: string | null;
+	lName: string | null;
+	email: string | null;
+	otherInfo: {
+		address: string | null;
+		state: string | null;
+		zip: string | null;
+		createdAt: string | null;
+	};
+}
 
-//declare global {
-//	namespace Express {
-//		export interface Request {
-//			currentUser?: UserPayload;
-//		}
-//	}
-//}
+declare global {
+	namespace Express {
+		export interface Request {
+			currentUser?: UserPayload;
+		}
+	}
+}
 
 export const currentUser = (req: Request, res: Response, next: NextFunction) => {
 	if (!req.session?.jwt) {
