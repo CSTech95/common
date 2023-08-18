d199aa25e8cc9ee99ab947c6e4761ee18aab1def
Aser <adocteur@gmail.com>
2023-08-18 12:06:09 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+export * from "./middlewares/current-user";
+export * from "./middlewares/require-auth";
+
 //import { User } from "./types/models/user";
 //export * from "./types/models/product";
 
-import { Product } from "./types/models/product";
-export { Product };
+//import { Product } from "./types/models/product";
+//export { Product };
